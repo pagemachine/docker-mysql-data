c044bf13398c543f28eacd644b1f7f9b9b9700ab
Mathias Brodala <mbrodala@pagemachine.de>
2024-06-28 12:43:33 +0200

    [TASK] Add test setup

---
+++ test/data/test_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE test_table (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255)
+);
+
+INSERT INTO test_table VALUES (1, "foo");
+INSERT INTO test_table VALUES (2, "bar");
---
+++ test/expected.sql
@@ -0,0 +1,10 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `test_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `test_table` VALUES (1,'foo');
+INSERT INTO `test_table` VALUES (2,'bar');
